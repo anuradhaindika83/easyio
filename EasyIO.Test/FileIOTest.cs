b3a92c2d492954b66d30fd898b190d0619e06f1e
Anuradha Amarasinghe <indikaanu83@gmail.com>
2019-01-07 18:06:07 +0530

    initial commit

---
+++ EasyIO.Test/FileIOTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EasyIO;
+using System.IO;
+
+namespace EasyIO.Test
+{
+    [TestClass]
+    public class FileIOTest
+    {
+        [TestMethod]
+        public void OpenReadTest()
+        {
+            string filename = "testdata\\test.txt";
+            var stream = filename.OpenRead();
+            Assert.IsNotNull(stream);
+            stream.Close();
+        }
+
+        [TestMethod]
+        public void OpenReadTestFailed()
+        {
+            string filename = "testdata\\invalidfile.txt";
+            Assert.ThrowsException<FileNotFoundException>(() => filename.OpenRead());
+            
+        }
+    }
+}
---
+++ EasyIO/FileIO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace EasyIO
+{
+    public static class FileIO
+    {
+        /// <summary>
+        /// Opens an existing file for reading. 
+        /// </summary>
+        /// <returns></returns>
+        public static FileStream OpenRead(this String name)
+        {
+            return File.Open(name, FileMode.Open);
+        }
+    }
+}
